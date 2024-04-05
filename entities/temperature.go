4383e3118a9718015146c0aa831098e547720c35
hieunguyen <hieuntg81@gmail.com>
2024-04-05 13:29:12 +0700

    DEV

--- entities/temperature.go
+++ entities/temperature.go
@@ -6,12 +6,23 @@ import (
 )
 
 type TemperatureRecord struct {
-	ID        string
-	Value     string
-	Unit      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	UpdatedBy string
+	ID        string    `db:"id"`
+	Value     string    `db:"value"`
+	Unit      string    `db:"unit"`
+	CreatedAt time.Time `db:"created_date"`
+	UpdatedAt time.Time `db:"last_modified_date"`
+	UpdatedBy string    `db:"last_modified_by"`
+}
+
+func NewTemperatureRecord(value, unit, updatedBy string) *TemperatureRecord {
+	return &TemperatureRecord{
+		ID:        uuid.New().String(),
+		Unit:      unit,
+		Value:     value,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		UpdatedBy: updatedBy,
+	}
 }
 
 func NewTemperatureRecord(value, unit, updatedBy string) *TemperatureRecord {
